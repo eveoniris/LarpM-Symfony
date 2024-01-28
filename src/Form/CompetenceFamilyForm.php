7016fb9114770fb96b089a3197c9c017f3acc474
Miyas <maxime.jumel@gmail.com>
2024-01-28 12:38:09 +0100

    Fix 500

--- src/Form/CompetenceFamilyForm.php
+++ src/Form/CompetenceFamilyForm.php
@@ -6,7 +6,6 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 
 /**
