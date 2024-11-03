3ce8d47171ad34372f19ab559e8b4b441afc8809
Laurent Semat <lsemat@manager.one>
2024-11-03 23:30:00 +0100

    fix personnage

--- src/Controller/PersonnageController.php
+++ src/Controller/PersonnageController.php
@@ -55,6 +55,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/personnage', name: 'personnage.')]
 class PersonnageController extends AbstractController
