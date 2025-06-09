78b653959f4e7f94c4a67faee72a500e914530a2
Laurent Semat <lsemat@manager.one>
2025-06-10 00:49:45 +0200

    add regle to ingredient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -1,33 +1,77 @@
 <?php
 
-
 namespace App\Controller;
 
 use App\Entity\Ingredient;
+use App\Enum\Role;
 use App\Form\IngredientDeleteForm;
 use App\Form\IngredientForm;
 use App\Repository\IngredientRepository;
+use App\Security\MultiRolesExpression;
 use App\Service\PagerService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[isGranted('ROLE_SCENARISTE')]
+#[IsGranted(new MultiRolesExpression(Role::SCENARISTE, Role::REGLE))]
 class IngredientController extends AbstractController
 {
     /**
-     * Liste des ingrédients.
+     * Ajoute un ingredient.
      */
-    #[Route('/ingredient', name: 'ingredient.list')]
-    public function adminListAction(Request $request, PagerService $pagerService, IngredientRepository $ingredientRepository)
+    #[Route('/ingredient/add', name: 'ingredient.add')]
+    public function adminAddAction(Request $request, EntityManagerInterface $entityManager)
     {
-        $pagerService->setRequest($request)->setRepository($ingredientRepository)->setLimit(50);
+        $ingredient = new Ingredient();
 
-        return $this->render('ingredient/list.twig', [
-            'pagerService' => $pagerService,
-            'paginator' => $ingredientRepository->searchPaginated($pagerService),
+        $form = $this->createForm(IngredientForm::class, $ingredient)
+            ->add('save', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Sauvegarder']);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $titre = $form->getData();
+
+            $entityManager->persist($titre);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'ingredient a été ajouté');
+
+            return $this->redirectToRoute('ingredient.detail', ['ingredient' => $ingredient->getId()], 303);
+        }
+
+        return $this->render('ingredient/add.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Supprime un ingredient.
+     */
+    #[Route('/ingredient/{ingredient}/delete', name: 'ingredient.delete')]
+    public function adminDeleteAction(Request $request, EntityManagerInterface $entityManager, Ingredient $ingredient)
+    {
+        $form = $this->createForm(IngredientDeleteForm::class, $ingredient)
+            ->add('save', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Supprimer']);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $titre = $form->getData();
+
+            $entityManager->remove($ingredient);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'L\'ingredient a été suprimé');
+
+            return $this->redirectToRoute('ingredient.list', [], 303);
+        }
+
+        return $this->render('ingredient/delete.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -43,32 +87,19 @@ class IngredientController extends AbstractController
     }
 
     /**
-     * Ajoute un ingredient.
+     * Liste des ingrédients.
      */
-    #[Route('/ingredient/add', name: 'ingredient.add')]
-    public function adminAddAction(Request $request,  EntityManagerInterface $entityManager)
-    {
-        $ingredient = new \App\Entity\Ingredient();
+    #[Route('/ingredient', name: 'ingredient.list')]
+    public function adminListAction(
+        Request $request,
+        PagerService $pagerService,
+        IngredientRepository $ingredientRepository,
+    ) {
+        $pagerService->setRequest($request)->setRepository($ingredientRepository)->setLimit(50);
 
-        $form = $this->createForm(IngredientForm::class, $ingredient)
-            ->add('save', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Sauvegarder']);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $titre = $form->getData();
-
-            $entityManager->persist($titre);
-            $entityManager->flush();
-
-           $this->addFlash('success', 'L\'ingredient a été ajouté');
-
-            return $this->redirectToRoute('ingredient.detail', ['ingredient' => $ingredient->getId()], 303);
-        }
-
-        return $this->render('ingredient/add.twig', [
-            'ingredient' => $ingredient,
-            'form' => $form->createView(),
+        return $this->render('ingredient/list.twig', [
+            'pagerService' => $pagerService,
+            'paginator' => $ingredientRepository->searchPaginated($pagerService),
         ]);
     }
 
@@ -76,7 +107,7 @@ class IngredientController extends AbstractController
      * Met à jour un ingredient.
      */
     #[Route('/ingredient/{ingredient}/update', name: 'ingredient.update')]
-    public function adminUpdateAction(Request $request,  EntityManagerInterface $entityManager, Ingredient $ingredient)
+    public function adminUpdateAction(Request $request, EntityManagerInterface $entityManager, Ingredient $ingredient)
     {
         $form = $this->createForm(IngredientForm::class, $ingredient)
             ->add('save', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Sauvegarder']);
@@ -89,40 +120,12 @@ class IngredientController extends AbstractController
             $entityManager->persist($ingredient);
             $entityManager->flush();
 
-           $this->addFlash('success', 'L\'ingredient a été sauvegardé');
+            $this->addFlash('success', 'L\'ingredient a été sauvegardé');
 
             return $this->redirectToRoute('ingredient.detail', ['ingredient' => $ingredient->getId()], 303);
         }
 
         return $this->render('ingredient/update.twig', [
-            'ingredient' => $ingredient,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * Supprime un ingredient.
-     */
-    #[Route('/ingredient/{ingredient}/delete', name: 'ingredient.delete')]
-    public function adminDeleteAction(Request $request,  EntityManagerInterface $entityManager, Ingredient $ingredient)
-    {
-        $form = $this->createForm(IngredientDeleteForm::class, $ingredient)
-            ->add('save', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, ['label' => 'Supprimer']);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $titre = $form->getData();
-
-            $entityManager->remove($ingredient);
-            $entityManager->flush();
-
-           $this->addFlash('success', 'L\'ingredient a été suprimé');
-
-            return $this->redirectToRoute('ingredient.list', [], 303);
-        }
-
-        return $this->render('ingredient/delete.twig', [
             'ingredient' => $ingredient,
             'form' => $form->createView(),
         ]);
