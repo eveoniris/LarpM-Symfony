26849460afc3685c83c9bcf626758583dc96579d
Laurent Sémat <lsemat@manager.one>
2026-03-04 12:29:31 +0100

    fix question repository

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -6,18 +6,13 @@ namespace App\Repository;
 
 use App\Entity\Participant;
 
-/**
- * LarpManager\Repository\QuestionRepository.
- *
- * @author kevin
- */
 class QuestionRepository extends BaseRepository
 {
     /**
      * Trouve toutes les questions auquel le participant n'a pas répondu.
      */
     /** @return array<int, \App\Entity\Question> */
-    public function findByParticipant(Participant $participant): array
+    public function findByParticipant(?Participant $participant): array
     {
         return $this->getEntityManager()->createQuery('SELECT q FROM App\Entity\Question q ORDER BY q.date DESC')->getResult();
     }
