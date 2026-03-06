250c1ffb606bb3f5a6ab7196e6f858a5d7fa0d53
Laurent Sémat <lsemat@manager.one>
2026-03-06 09:25:10 +0100

    fix: NoblesseService::remove() use correct valuesMap instead of getId()+1

--- src/Service/Competence/NoblesseService.php
+++ src/Service/Competence/NoblesseService.php
@@ -32,9 +32,18 @@ class NoblesseService extends CompetenceService
     protected function remove(): void
     {
         $level = $this->getCompetence()->getLevel();
-        $value = $level?->getId() + 1;
 
-        if ($level && $value > 1 && $value < 7) {
+        $valuesMap = [
+            1 => 2,
+            2 => 3,
+            3 => 2,
+            4 => 5,
+            5 => 6,
+        ];
+
+        $value = $valuesMap[$level?->getId()] ?? 0;
+
+        if ($level && $value > 0) {
             $this->removeRenomme($value, \sprintf('[Retrait] %s niveau %s', $this->getCompetence()->getCompetenceFamily()?->getLabel(), $level->getLabel()));
         }
     }
